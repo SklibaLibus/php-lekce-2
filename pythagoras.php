e666d98c52c50ef8411406be887ca12f9b9af396
KristinaT <trojakovakristina@gmail.com>
2019-10-11 21:11:20 +0200

    přidán obsah trojúhelníku

--- pythagoras.php
+++ pythagoras.php
@@ -1,5 +1,17 @@
 <?php
-$a = 9;
-$b = 12;
+
+// obsah obdelníku
+$a = 5;
+$b = 4;
 $obdelnikobsah = $a * $b;
-echo "Obsah obdelníku o stranách $a cm a $b cm je: $obdelnikobsah cm2.";
+
+// obsah trojúhelníku
+$strana = 6;
+$uhel = 60;
+$vyska = sin(deg2rad(60)) * $strana;
+$trojuhelnikobsah = round($vyska * $strana / 2);
+?>
+
+Obsah obdelníku o stranách <?php echo $a; ?>cm a <?php echo $b; ?>cm je <?php echo $obdelnikobsah; ?>cm².<br>
+
+Obsah rovnostranného trojúhelníku o straně délky <?php echo $strana; ?>cm a úhlu <?php echo $uhel; ?>° je <?php echo $trojuhelnikobsah; ?>cm².
